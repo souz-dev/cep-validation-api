4901a79fd995872fef4a43c0f9b70dd9a62c82fa
souz-dev <souz.developer@gmail.com>
2025-01-27 18:47:35 -0300

    feat(Data): ensure AddresRepository

---
+++ src/data/protocols/address-repository.ts
@@ -0,0 +1,18 @@
+import { AddressModel } from "@/domain/usecases/models/addres";
+
+export type AddressData = {
+  name: string;
+  email: string;
+  cep: string;
+  state: string;
+  city: string;
+  street: string;
+  district: string;
+};
+
+export interface AddressRepository {
+  create: (data: AddressData) => Promise<AddressModel>;
+  findAll: () => Promise<AddressModel[]>;
+  findById: (id: string) => Promise<AddressModel | null>;
+  update: (id: string, data: AddressData) => Promise<AddressModel>;
+}
