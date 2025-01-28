6c60b4897743dafc253749cc2d89311c503207b3
souz-dev <souz.developer@gmail.com>
2025-01-27 22:41:13 -0300

    feat(config): ensure middlewares  and used in app

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,8 @@
 import express from "express";
-
+import setupMiddlewares from "./middlewares";
 const app = express();
 
+setupMiddlewares(app);
 // Middleware para definir o tipo de conteúdo padrão como JSON
 app.use((req, res, next) => {
   res.type("json");
