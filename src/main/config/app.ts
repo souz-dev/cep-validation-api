dd61d7270b5f264f7a4bb6580ea740c0ede50a2e
souz-dev <souz.developer@gmail.com>
2025-01-27 22:23:30 -0300

    fix(middleware): ensure default content type is json and add test for xml content type

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,4 +2,12 @@ import express from "express";
 
 const app = express();
 
+// Middleware para definir o tipo de conteúdo padrão como JSON
+app.use((req, res, next) => {
+  res.type("json");
+  next();
+});
+
+app.use(express.json());
+
 export default app;
